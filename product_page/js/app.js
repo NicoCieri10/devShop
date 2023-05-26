b07955bc4f093b37ff80fdfaea5b9c83b8919870
Luana Naomi Ruffinelli <luanaruffinelli@gmail.com>
2023-05-26 12:36:52 -0300

    html, css y js

---
+++ product_page/js/app.js
@@ -0,0 +1,153 @@
+const bodyContainer = document.getElementById('.main-container');
+const productContainer = document.createDocumentFragment('.product__container');
+const cartElement = document.getElementById('cart');
+const footerElement = document.getElementById('footer');
+const templateElement = document.getElementById('template');
+const templateFooterElement = document.getElementById('templateFooter');
+
+// const getProduct = async () => {
+//   try {
+//     const data = await ;
+//     const info = await data.json;
+//     showProduct(info);
+//   } catch (error) {
+//     console.log('error');
+// };
+
+// getProduct();
+
+// const showProduct = (info) => {
+//   info.results((item) => {
+//     const clone = productContainer.content.cloneNode(true);
+//     clone.querySelector('.gallery__image-container').src = item.image;
+//     clone.querySelector('.gallery__image-container').alt = `${item.name} ${item.id}`;
+//     clone.querySelector('.details__company').textContent = item.company;
+//     clone.querySelector('.details__title').textContent = item.name;
+//     clone.querySelector('.details__description').textContent = item.descriprtion;
+//     clone.querySelector('.details__price').textContent = item.price;
+
+//     productContainer.appendChild(clone);
+//   });
+//   bodyContainer.appendChild(productContainer);
+// }
+
+let cart = [];
+
+function renderCart() {
+  cartElement.innerHTML = '';
+
+  cart.forEach((product, index) => {
+    const template = templateElement.content.cloneNode(true);
+
+    const productNameElement = template.querySelector('.lead');
+    productNameElement.textContent = product.name + " $" + product.price + " x " + product.quantity;
+
+    const increaseButton = template.querySelector('.btn-success');
+    increaseButton.addEventListener('click', () => {
+      increaseQuantity(index);
+    });
+
+    const decreaseButton = template.querySelector('.btn-danger');
+    decreaseButton.addEventListener('click', () => {
+      decreaseQuantity(index);
+    });
+
+    cartElement.appendChild(template);
+  });
+
+  updateFooter();
+}
+
+function increaseQuantity(index) {
+  cart[index].quantity++;
+  renderCart();
+  saveCartToLocalStorage();
+}
+
+function decreaseQuantity(index) {
+  if (cart[index].quantity > 1) {
+    cart[index].quantity--;
+  } else {
+    cart.splice(index, 1);
+  }
+  renderCart();
+  saveCartToLocalStorage();
+}
+
+function updateFooter() {
+  const total = cart.reduce((accumulator, product) => {
+    return accumulator + product.price * product.quantity;
+  }, 0);
+
+  footerElement.innerHTML = '';
+
+  if (cart.length > 0) {
+    const template = templateFooterElement.content.cloneNode(true);
+
+    const totalPriceElement = template.querySelector('.cart__card-price');
+    totalPriceElement.textContent = total.toFixed(2);
+
+    footerElement.appendChild(template);
+  }
+}
+
+function saveCartToLocalStorage() {
+  localStorage.setItem('cart', JSON.stringify(cart));
+}
+
+function loadCartFromLocalStorage() {
+  const cartData = localStorage.getItem('cart');
+  if (cartData) {
+    cart = JSON.parse(cartData);
+    renderCart();
+  }
+}
+
+const addButton = document.querySelector('.details__button');
+addButton.addEventListener('click', () => {
+  const productName = addButton.getAttribute('data-product');
+  const productPrice = parseFloat(addButton.getAttribute('data-price'));
+
+  const existingProduct = cart.find((product) => product.name === productName);
+
+  if (existingProduct) {
+    existingProduct.quantity++;
+  } else {
+    cart.push({ name: productName, price: productPrice, quantity: 1 });
+  }
+
+  renderCart();
+  saveCartToLocalStorage();
+});
+
+const suggestionButtons = document.querySelectorAll('.suggestions__card-button');
+
+suggestionButtons.forEach((button) => {
+  button.addEventListener('click', () => {
+    const productName = button.getAttribute('data-product');
+    const productPrice = parseFloat(button.getAttribute('data-price'));
+
+    const existingProduct = cart.find((product) => product.name === productName);
+
+    if (existingProduct) {
+      existingProduct.quantity++;
+    } else {
+      cart.push({ name: productName, price: productPrice, quantity: 1 });
+    }
+
+    renderCart();
+    saveCartToLocalStorage();
+  });
+});
+
+loadCartFromLocalStorage();
+
+
+
+// var answer = window.confirm("Para agregar productos al carrito es necesario iniciar sesión");
+
+// if (answer) {
+//   window.location.href = ;
+// } else {
+//     window.alert();
+// }
